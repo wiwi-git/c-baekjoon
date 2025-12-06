d7ff6d63d022a6a647c3d610b6bca0fa7c1b5163
wiwi-git <duse34@hotmail.com>
2025-12-07 05:50:37 +0900

    feat: q2558 문제

---
+++ q2558/q2558/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  q2558
+//
+//  Created by 위대연 on 12/7/25.
+//
+
+#include <stdio.h>
+/**
+ q2558 A+B - 2
+ */
+int main(int argc, const char * argv[]) {
+    int a = 0;
+    int b = 0;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    printf("%d\n", a+b);
+    return 0;
+}
